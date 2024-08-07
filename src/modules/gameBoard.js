76bf5b246d95249fbea614d6feead35aa0cc5b4c
MetalSnow <aymanetrend@gmail.com>
2024-08-07 23:22:51 +0800

    feat: implement new methods to the gameBoard class
    -placeShips: places ships at specific coordinates
    -receiveAttack: determines whether or not the attack hit a ship
    -reportSunkships: report whether or not all of their ships have been sunk

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -1,6 +1,7 @@
 export default class Gameboard {
   constructor() {
     this.board = [];
+    this.ships = [];
   }
 
   generateBoard() {
@@ -10,5 +11,58 @@ export default class Gameboard {
         this.board[i][j] = '';
       }
     }
+  }
+
+  getBoard() {
+    return this.board;
+  }
+
+  placeShips(ship, coord, orientation) {
+    const [x, y] = coord;
+
+    if (orientation === 'horizontal' && x + ship.length > this.board.length) {
+      throw new Error('Ship goes out of bounds horizontally.');
+    }
+    if (orientation === 'vertical' && y + ship.length > this.board[0].length) {
+      throw new Error('Ship goes out of bounds vertically.');
+    }
+
+    // Store ships in ship array
+    this.ships.push(ship);
+
+    for (let i = 0; i < ship.length; i++) {
+      if (orientation === 'horizontal') {
+        if (this.board[x][y + i] === '') {
+          this.board[x][y + i] = ship;
+        } else {
+          return 'Place is already full';
+        }
+      } else {
+        if (this.board[x + i][y] === '') {
+          this.board[x + i][y] = ship;
+        } else {
+          return 'Place is already full';
+        }
+      }
+    }
+  }
+
+  receiveAttack(coord) {
+    const [x, y] = coord;
+
+    if (this.board[x][y] !== '') {
+      this.board[x][y].hit();
+      this.board[x][y] = 1;
+    } else {
+      this.board[x][y] = 0;
+    }
+
+    if (this.reportSunkships()) {
+      return 'Game Finished';
+    }
+  }
+
+  reportSunkships() {
+    return this.ships.every((ship) => ship.sunk === true);
   }
 }
--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -1,11 +1,45 @@
 import Gameboard from '../modules/gameBoard';
+import Ship from '../modules/ship';
 
 describe('Game Board', () => {
   const game = new Gameboard();
+  game.generateBoard();
 
   test('Board generated', () => {
-    game.generateBoard();
-
     expect(game.board.length).toEqual(10);
+  });
+
+  const ship = new Ship(3);
+  const ship2 = new Ship(2);
+
+  test('place ships', () => {
+    game.placeShips(ship, [2, 3], 'horizontal');
+
+    expect(game.board[2][3]).toEqual(ship);
+    expect(game.board[2][4]).toEqual(ship);
+    expect(game.board[2][5]).toEqual(ship);
+
+    game.placeShips(ship2, [7, 4], 'vertical');
+
+    expect(game.board[7][4]).toEqual(ship2);
+    expect(game.board[8][4]).toEqual(ship2);
+  });
+
+  test('receive attack', () => {
+    game.receiveAttack([2, 3]);
+    game.receiveAttack([2, 4]);
+    game.receiveAttack([2, 5]);
+
+    expect(ship.hitsReceived).toBe(3);
+    expect(ship.sunk).toBe(true);
+
+    expect(game.board[2][3]).toBe(1);
+    expect(game.board[2][4]).toBe(1);
+    expect(game.board[2][5]).toBe(1);
+
+    // check for missed attacks
+    game.receiveAttack([4, 6]);
+
+    expect(game.board[4][6]).toBe(0);
   });
 });
