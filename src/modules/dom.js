99da59baceaded638a7072c27ad9a51898e23e83
MetalSnow <aymanetrend@gmail.com>
2024-08-14 23:06:12 +0800

    feat(gameController, dom): Integrate game board rendering and ship placement
    - Implemented `gameController` to handle the game's main logic, including player and CPU initialization, ship placement, and turn-based gameplay.
    - Added `highlightSurrounding` function in `gameController` to visually mark surrounding cells after a hit.
    - Integrated event listeners in `gameController` to handle player attacks on the CPU board.
    - Refined the `dom` module to manage DOM manipulation, including rendering game boards, displaying ships, and handling UI interactions.
    - Included logic in `dom` to randomize player ship placement and start the game.

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,23 +1,51 @@
-export { changeDisplay, startBtn };
+import { game } from './gameController';
+
+export {
+  changeDisplay,
+  startBtn,
+  playerBoardDiv,
+  computerBoardDiv,
+  renderBoard,
+  renderShips,
+  randomizeBtn,
+};
+
 const startBtn = document.querySelector('#start-game');
 const homeDiv = document.querySelector('.home-page');
 const gameContainer = document.querySelector('.game-container');
-const playerBoard = document.querySelector('.player-board');
-const computerBoard = document.querySelector('.computer-board');
+const playerBoardDiv = document.querySelector('.player-board');
+const computerBoardDiv = document.querySelector('.computer-board');
+const randomizeBtn = document.querySelector('#random');
+const playBtn = document.querySelector('#play-game');
 
-function changeDisplay() {
+playBtn.addEventListener('click', () => {
+  game.playGame();
+});
+
+const changeDisplay = () => {
   homeDiv.style.display = 'none';
   gameContainer.style.display = 'flex';
-}
+};
 
-function createGameBoard(div) {
-  for (let i = 0; i < 100; i++) {
-    const cell = document.createElement('button');
-    cell.className = 'cell';
-    cell.dataset.cell = i;
-    div.appendChild(cell);
+const renderBoard = (div, gameBoard, name) => {
+  for (let i = 0; i < gameBoard.length; i++) {
+    for (let j = 0; j < gameBoard[i].length; j++) {
+      const cell = document.createElement('button');
+      cell.classList.add(`${name}-cell`);
+      cell.dataset.row = i;
+      cell.dataset.col = j;
+      div.appendChild(cell);
+    }
   }
-}
+};
 
-createGameBoard(playerBoard);
-createGameBoard(computerBoard);
+const renderShips = (name, board) => {
+  const cells = document.querySelectorAll(`.${name}-cell`);
+
+  cells.forEach((cell) => {
+    let [x, y] = [cell.dataset.row, cell.dataset.col];
+    if (board[x][y] !== '' && board[x][y] !== null) {
+      cell.style = 'background-color: #2f4f4fdc';
+    }
+  });
+};
--- src/modules/gameController.js
+++ src/modules/gameController.js
@@ -1,18 +1,131 @@
 import humanPlayer from './humanPlayer';
 import computerPlayer from './computerPlayer';
+import hitIcon from '../icons/hit.png';
+import missedIcon from '../icons/missed.png';
+import {
+  computerBoardDiv,
+  playerBoardDiv,
+  randomizeBtn,
+  renderBoard,
+  renderShips,
+} from './dom';
 
-export default function gameController() {
+export { game };
+
+const game = gameController();
+
+function gameController() {
   const player = new humanPlayer();
   const cpu = new computerPlayer();
 
-  player.gameBoard.placeAllShips();
-  cpu.gameBoard.placeAllShips();
+  const playerBoard = player.gameBoard.getBoard();
+  const cpuBoard = cpu.gameBoard.getBoard();
 
-  console.log(player.gameBoard.board);
-  console.log(cpu.gameBoard.board);
+  const startGame = () => {
+    renderBoard(playerBoardDiv, playerBoard, 'player');
+    renderBoard(computerBoardDiv, cpuBoard, 'cpu');
 
-  const newGame = () => {
-    cpu.switchTurn();
+    computerBoardDiv.style.opacity = '0.3';
+    computerBoardDiv.style.pointerEvents = 'none';
+
+    player.gameBoard.placeAllShips();
+    cpu.gameBoard.placeAllShips();
+
+    renderShips('player', playerBoard);
+
+    randomizeBtn.addEventListener('click', () => {
+      console.log('randomize');
+      while (playerBoardDiv.firstChild) {
+        playerBoardDiv.removeChild(playerBoardDiv.firstChild);
+      }
+
+      player.randomizeShips();
+      renderBoard(playerBoardDiv, player.gameBoard.getBoard(), 'player');
+      renderShips('player', player.gameBoard.getBoard());
+    });
   };
-  newGame();
+
+  const playGame = () => {
+    cpu.switchTurn();
+
+    computerBoardDiv.style.opacity = '';
+    computerBoardDiv.style.pointerEvents = '';
+    computerBoardDiv.style.border = '2px solid yellow';
+
+    playerBoardDiv.style.pointerEvents = 'none';
+
+    const cpuCells = document.querySelectorAll('.cpu-cell');
+    cpuCells.forEach((cell) => {
+      cell.addEventListener('click', () => {
+        let [x, y] = [cell.dataset.row, cell.dataset.col];
+
+        const hitImg = new Image();
+        const missedImg = new Image();
+
+        hitImg.src = hitIcon;
+        missedImg.src = missedIcon;
+
+        hitImg.style.width = '25px';
+        missedImg.style.width = '25px';
+
+        if (typeof cpuBoard[x][y] === 'object' && cpuBoard[x][y] !== null) {
+          cell.appendChild(hitImg);
+          cell.style.backgroundColor = '#ff000054';
+          highlightSurrounding(x, y);
+        } else {
+          cell.appendChild(missedImg);
+          cell.style.backgroundColor = '#ffff0045';
+        }
+
+        cell.style.pointerEvents = 'none';
+
+        player.sendAttack(cpu.gameBoard, [x, y]);
+        player.switchTurn();
+        cpu.switchTurn();
+      });
+    });
+
+    if (cpu.getIsTurn()) {
+    }
+  };
+
+  return { startGame, playGame };
 }
+
+const highlightSurrounding = (x, y, ship) => {
+  const spots = [
+    [-1, -1],
+    [-1, 1],
+    [1, -1],
+    [1, 1],
+  ];
+
+  spots.forEach((spot) => {
+    const row = +x + spot[0];
+    const col = +y + spot[1];
+
+    console.log(row);
+    console.log(col);
+
+    if (col >= 0 && col < 10 && row >= 0 && row < 10) {
+      const cell = document.querySelector(
+        `.cpu-cell[data-col="${col}"][data-row="${row}"]`
+      );
+
+      if (!cell.firstChild) {
+        const missedImg = new Image();
+        missedImg.src = missedIcon;
+        missedImg.style.width = '25px';
+
+        cell.style.pointerEvents = 'none';
+        cell.style.backgroundColor = '#80808046';
+        missedImg.style.opacity = '0.7';
+
+        cell.appendChild(missedImg);
+      }
+    }
+  });
+
+  if (ship.sunk) {
+  }
+};
