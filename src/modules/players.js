1572dda3d2c6e0b5b5009a0e7107ed0330c6adc5
MetalSnow <aymanetrend@gmail.com>
2024-08-14 23:03:48 +0800

    Feat: Add randomizeShips method
    to change ships places randomly

--- src/modules/players.js
+++ src/modules/players.js
@@ -20,4 +20,14 @@ export default class Player {
   switchTurn() {
     this.isturn = this.isturn === true ? false : true;
   }
+
+  randomizeShips() {
+    const board = new Gameboard();
+    board.generateBoard();
+
+    board.ships = [];
+    board.placeAllShips();
+
+    this.gameBoard = board;
+  }
 }
